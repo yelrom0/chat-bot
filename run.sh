80b8104cfb832ad736e2a622bc1c9617acdd7d59
Daniel Morley <daniel@cognativ.com.au>
2022-08-05 01:47:28 +1000

    feat(Run Script): hypercorn reload on change

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 # This script is used to start hypercorn.
 # Hypercorn is running with QUIC http/3 on port 4433. But also runs on port 8000 by default.
 
-hypercorn api.main:app --quic-bind localhost:4433
+hypercorn api.main:app --quic-bind localhost:4433 --reload
