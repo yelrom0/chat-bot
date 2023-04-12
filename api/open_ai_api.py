8a39b9192cc1dc546c494c2c0dea74ed1b8ded87
Daniel Morley <yelrom0@gmail.com>
2023-04-13 03:25:04 +1000

    feat(Open AI API): Wrestlebot is now active

--- api/open_ai_api.py
+++ api/open_ai_api.py
@@ -18,28 +18,27 @@ class AIApi:
 
         # Constants
         self.FIRST_OUTPUT = (
-            "Hello, I am Sportsbot, ask me a question about sports."  # noqa: E501
+            "Hello, I am Wrestlebot, ask me a question about wrestling."  # noqa: E501
         )
-        self.SYSTEM_PROMPT = "You are a friendly yet laconic sports commentator. When asked a question, you respond with a short, witty answer with a link to more information."  # noqa: E501
-        self.messages = [{"role": "system", "content": self.SYSTEM_PROMPT}]
+        self.SYSTEM_PROMPT = "You are a friendly yet laconic wrestling commentator. When asked a question, you respond with a short, witty answer with a link to more information."  # noqa: E501
+        self.messages = [{"role": "system", "content": f"{self.SYSTEM_PROMPT}"}]
         self.chat_log = f"{self.FIRST_OUTPUT}\n"
         self.FILE = open("debug_log.txt", "w")
 
         # init openai interface
         openai.api_key = self.env["OPENAI_API_KEY"]
-        self.chat_completion = openai.ChatCompletion(engine="gpt-3.5-turbo")
+        # self.chat_completion = openai.ChatCompletion(engine="gpt-3.5-turbo")
 
-    def get_response(self, text: str) -> str:
+    async def get_response(self, text: str) -> str:
         # get response from openai interface
         # prompt = f"{self.chat_log}\n{text}\n"
-        messages = self.messages.append({"role": "user", "content": text})
-        response = ""
-        # loop to stop blank responses
-        while not response:
-            response = self.chat_completion.create(
-                messages=messages,
-                temperature=0,
-            )
-            response = response["choices"][0]["message"]["content"]
-        self.messages.append({"role": "system", "content": response})
+        self.messages.append({"role": "user", "content": f"{text}"})
+
+        response = await openai.ChatCompletion.acreate(
+            model="gpt-3.5-turbo",
+            messages=self.messages,
+            temperature=0,
+        )
+        response = response.choices[0].message.content
+        self.messages.append({"role": "system", "content": f"{response}"})
         return response
